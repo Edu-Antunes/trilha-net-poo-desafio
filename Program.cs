40a4cc7b1c1072fbd7fd64b1cd0a5781fd2e93a3
Eduardo Antunes <eduardo.antunes@verithus.com.br>
2023-10-14 07:33:30 -0300

    Alteração na classe Smartphone,nos metodos Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Console.WriteLine("************************");
+Smartphone nokia = new Nokia(numero: "(11) 11111-1111", modelo: "Nokia C01 Plus 32GB", imei: "1111111", memoria: "4G");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
+Console.WriteLine("\n");
+
+Console.WriteLine("**************-------------***************----------**********");
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Console.WriteLine("************************");
+Smartphone iphone = new Iphone(numero: "(11) 11111-1111", modelo: "Iphone 14", imei: "1111111", memoria: "4G");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+
